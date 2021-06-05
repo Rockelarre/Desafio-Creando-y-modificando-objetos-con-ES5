99d313de2d8115ed0a7728c77eec52db095cc690
Rockelarre <leogrisesgmail@example.com>
2021-06-05 19:22:53 -0400

    dev_1

--- script.js
+++ script.js
@@ -14,53 +14,82 @@ function Paciente(nombre,edad,rut,diagnostico){
     var _rut = rut;
     var _diagnostico = diagnostico;
 
-    // Getter y Setter para propiedad _nombre
-    Object.defineProperty(this,'getNombre',{
+
+    Object.defineProperty(this,'_getNombre',{
         get: function(){
             return _nombre;
         }
     });
-    Object.defineProperty(this,'setNombre',{
+    Object.defineProperty(this,'_setNombre',{
         set: function(nombre){
             _nombre = nombre;
         }
     });
 
-    // Getter y Setter para propiedad _edad
-    Object.defineProperty(this,'getEdad',{
+
+    Object.defineProperty(this,'_getEdad',{
         get: function(){
             return _edad;
         }
     });
-    Object.defineProperty(this,'setEdad',{
+    Object.defineProperty(this,'_setEdad',{
         set: function(edad){
             _edad = edad;
         }
     });
 
-    // Getter y Setter para propiedad _rut
-    Object.defineProperty(this,'getRut',{
+
+
+    Object.defineProperty(this,'_getRut',{
         get: function(){
             return _rut;
         }
     });
-    Object.defineProperty(this,'setRut',{
+    Object.defineProperty(this,'_setRut',{
         set: function(rut){
             _rut = rut;
         }
     });
 
-    // Getter y Setter para propiedad _diagnostico
-    Object.defineProperty(this,'getDiagnostico',{
+
+    Object.defineProperty(this,'_getDiagnostico',{
         get: function(){
             return _diagnostico;
         }
     });
-    Object.defineProperty(this,'setDiagnostico',{
+    Object.defineProperty(this,'_setDiagnostico',{
         set: function(diagnostico){
             _diagnostico = diagnostico;
         }
     });
+}
+
+Paciente.prototype.getNombre = function(){
+    return this._getNombre;
+}
+Paciente.prototype.setNombre = function(nombre){
+    this._setNombre = nombre;
+}
+
+Paciente.prototype.getEdad = function(){
+    return this._getEdad;
+}
+Paciente.prototype.setEdad = function(edad){
+    this._setEdad = edad;
+}
+
+Paciente.prototype.getRut = function(){
+    return this._getRut;
+}
+Paciente.prototype.setRut = function(rut){
+    this._setRut = rut;
+}
+
+Paciente.prototype.getDiagnostico = function(){
+    return this._getDiagnostico;
+}
+Paciente.prototype.setDiagnostico = function(diagnostico){
+    this._setDiagnostico = diagnostico;
 }
 
 // Método para mostrar todos los pacientes y sus datos
@@ -70,7 +99,7 @@ Consultorio.prototype.mostrarPacientes = function(){
     // Ciclo FOR para recorre todo el arreglo de pacientes del consultorio e
     // imprimir por consola todos los datos.
     for(var i=0; i < this.pacientes.length; i++){
-        console.log('Nombre: ' + this.pacientes[i].getNombre + ' - ' + 'Edad: ' + this.pacientes[i].getEdad + ' - ' + 'Rut: ' + this.pacientes[i].getRut + ' - ' + 'Diagnóstico: ' + this.pacientes[i].getDiagnostico);
+        console.log('Nombre: ' + this.pacientes[i].getNombre() + ' - ' + 'Edad: ' + this.pacientes[i].getEdad() + ' - ' + 'Rut: ' + this.pacientes[i].getRut() + ' - ' + 'Diagnóstico: ' + this.pacientes[i].getDiagnostico());
     }
 }
 
@@ -98,4 +127,4 @@ var c1 = new Consultorio('Consultorio Capsule Corp',[p1,p2,p3]);
 c1.mostrarPacientes();
 
 // Llamada a método para buscar por nombre
-c1.buscarDatosPorNombre('Gokú');
+/* c1.buscarDatosPorNombre('Gokú'); */
